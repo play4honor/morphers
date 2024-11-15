c4cd4970937b34f9efd041557a3952ba29474577
DoubleSec <awzack@gmail.com>
2024-11-14 23:02:28 -0600

    add mi quantiler to init

--- morphers/__init__.py
+++ morphers/__init__.py
@@ -1,4 +1,9 @@
 from .morphers.categorical import Integerizer, BigIntegerizer
-from .morphers.continuous import Normalizer, RankScaler, Quantiler
+from .morphers.continuous import (
+    Normalizer,
+    RankScaler,
+    Quantiler,
+    MissingIndicatorQuantiler,
+)
 
 __version__ = "0.1.2"
